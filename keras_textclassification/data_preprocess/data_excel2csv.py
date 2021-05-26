330d7f58fad5bda0b72fe74823e478d43c1ecff3
atom-zh <xiaolongatom@gmail.com>
2021-05-26 23:26:31 +0800

    add removePunctuation

--- keras_textclassification/data_preprocess/data_excel2csv.py
+++ keras_textclassification/data_preprocess/data_excel2csv.py
@@ -13,22 +13,35 @@ import numpy as np
 import json
 import os
 
+def removePunctuation(content):
+    """
+    文本去标点
+    """
+    punctuation = r"~!@#$%^&*()_+`{}|\[\]\:\";\-\\\='<>?,./，。、《》？；：‘“{【】}|、！@#￥%……&*（）——+=-"
+    content = re.sub(r'[{}]+'.format(punctuation), '', content)
+
+    if content.startswith(' ') or content.endswith(' '):
+        re.sub(r"^(\s+)|(\s+)$", "", content)
+    return content.strip()
+
 def excel2csv():
-    data = pd.read_excel(os.path.dirname(path_multi_label_train)+'/01-anhui.xlsx')
     labels = []
     trains = ['label|,|ques']
+    data = pd.read_excel(os.path.dirname(path_multi_label_train)+'/01-anhui.xlsx')
+    s_list = data.tolist()
     print(data.values)
-
+    for line in s_list:
+        print(line)
 
 
     # 生成 label 文件
-    with open(path_multi_label_valid) as f_label:
+    with open(path_multi_label_valid, 'w', encoding='utf-8') as f_label:
         for line in labels:
             labels.write(line + '\n')
         f_label.close()
 
     # 生成 train.csv 文件
-    with open(path_multi_label_train) as f_train:
+    with open(path_multi_label_train, 'w', encoding='utf-8') as f_train:
         for line in trains:
             f_train.write(line + '\n')
         f_train.close()
